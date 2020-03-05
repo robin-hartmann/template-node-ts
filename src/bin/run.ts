d0c3a1c8f94098ae766ae612d19ff74b4f353316
Robin Hartmann <contact.robin.hartmann@gmail.com>
2020-03-06 00:46:04 +0100

    Re-enable eslint rule `no-console`

--- src/bin/run.ts
+++ src/bin/run.ts
@@ -1,10 +1,12 @@
 import { promisedFiglet } from '../lib/lib'
 
 export const run = async (): Promise<void> => {
+  /* eslint-disable no-console */
   try {
     console.log(await promisedFiglet('Hello World!'))
   } catch (error) {
     console.error('Error while creating ASCII art:')
     console.error(error)
   }
+  /* eslint-enable no-console */
 }
