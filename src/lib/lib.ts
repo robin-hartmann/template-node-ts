266a13e2e5f9e16850255a9566cc106b3009e651
Robin Hartmann <contact.robin.hartmann@gmail.com>
2020-03-24 19:24:11 +0100

    Update prettier and improve rules
    - update prettier to 2.0.2
    - set rule `trailingComma` to `all`

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -2,7 +2,7 @@ import figlet from 'figlet'
 
 function promisedFiglet(txt: string): Promise<string> {
   return new Promise((resolve, reject) =>
-    figlet(txt, (error, result) => (error ? reject(error) : resolve(result)))
+    figlet(txt, (error, result) => (error ? reject(error) : resolve(result))),
   )
 }
 
